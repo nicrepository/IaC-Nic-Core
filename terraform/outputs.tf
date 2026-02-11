78a5551630d49e10e4dc5d4043456c1c78d92e19
djalv <alvaroneto98@protonmail.com>
2026-02-10 21:52:26 -0300

    fix: Corrigido os IPs das VMs no ansible_inventory

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,14 +2,16 @@ resource "local_file" "ansible_inventory" {
   content = <<EOF
 
   [firewalls]
-  # IP fixo manual, pois o pfSense não tem agente QEMU ativo para reportar IP
-  192.168.50.1 ansible_user=admin ansible_password=pfsense # Senha padrao, mude logo!
+  # IP da LAN Gerência (vmbr0) definido na instalação
+  10.10.10.1 ansible_user=admin ansible_password='SUA_SENHA_FORTE'
 
   [siem]
-  ${proxmox_vm_qemu.srv_wazuh.default_ipv4_address} ansible_user=nic-core ansible_ssh_private_key_file=~/.ssh/id_rsa
+  # IP definido no main.tf (srv_wazuh)
+  10.10.10.3 ansible_user=nic-core ansible_ssh_private_key_file=~/.ssh/id_rsa
 
   [apps]
-  ${proxmox_vm_qemu.srv_apps.default_ipv4_address} ansible_user=nic-core ansible_ssh_private_key_file=~/.ssh/id_rsa
+  # IP definido no main.tf (srv_apps)
+  10.10.10.4 ansible_user=nic-core ansible_ssh_private_key_file=~/.ssh/id_rsa
   EOF
-  filename = "./inventory.ini"
+  filename = "../ansible/inventory.ini"
 }
